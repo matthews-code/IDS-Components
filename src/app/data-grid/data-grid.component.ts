9b3146eb71a8a66946b64a78a014e9abe91db845
matthews-code <matthew_buensalida@dlsu.edu.ph>
2023-10-19 17:34:58 +0800

    raw data manipulation for datagrid

--- src/app/data-grid/data-grid.component.ts
+++ src/app/data-grid/data-grid.component.ts
@@ -1,23 +1,73 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import type IdsDataGrid from 'ids-enterprise-wc/components/ids-data-grid/ids-data-grid';
 import type { IdsDataGridColumn } from 'ids-enterprise-wc/components/ids-data-grid/ids-data-grid-column';
 import sampleData from './sample-data.json';
+import unformattedJson from './unformatted-springboot.json';
 
 @Component({
   selector: 'app-data-grid',
   templateUrl: './data-grid.component.html',
   styleUrls: ['./data-grid.component.css'],
 })
-export class DataGridComponent implements AfterViewInit {
+export class DataGridComponent implements AfterViewInit, OnInit {
   constructor() {}
 
   @ViewChild('dataGridEditable') dataGrid!: ElementRef<IdsDataGrid>;
   columns: IdsDataGridColumn[] = [];
+  formattedData: { [key: string]: any }[] = [];
 
-  // get sheet data from endpoint here
+  maxObjectsInCell = 0;
+
+  ngOnInit(): void {
+    // const formattedData = [];
+    const aUnicode = 97;
+    // get max number of columns to push in data grid component
+    for (let row of unformattedJson.rows) {
+      if (row.cells.length > this.maxObjectsInCell) {
+        this.maxObjectsInCell = row.cells.length;
+      }
+    }
+
+    // push all columns to datagrid columns
+    for (let i = 0; i < this.maxObjectsInCell; i++) {
+      let text = String.fromCharCode(i + aUnicode);
+      this.columns.push({
+        id: text,
+        name: text,
+        field: text,
+        sortable: true,
+        resizable: true,
+        reorderable: true,
+        // editor: {
+        //   type: 'input',
+        //   inline: true,
+        //   editorSettings: {
+        //     autoselect: true,
+        //     dirtyTracker: true,
+        //   },
+        // },
+      });
+    }
+
+    // reformat raw data
+    for (let row of unformattedJson.rows) {
+      let formattedRow: { [key: string]: any } = {};
+      for (let cell of row.cells) {
+        formattedRow[String.fromCharCode(Number(cell.id) - 1 + aUnicode)] =
+          cell.displayValue;
+      }
+      this.formattedData.push(formattedRow);
+    }
+  }
 
   ngAfterViewInit(): void {
-    this.columns.push({
+    this.columns.unshift({
       id: 'selectionCheckbox',
       name: 'selection',
       sortable: false,
@@ -25,84 +75,6 @@ export class DataGridComponent implements AfterViewInit {
       formatter: this.dataGrid.nativeElement.formatters.selectionCheckbox,
       align: 'center',
     });
-    this.columns.push({
-      id: 'create',
-      name: '',
-      field: 'create',
-      sortable: true,
-      resizable: true,
-      reorderable: true,
-      formatter: this.dataGrid.nativeElement.formatters.text,
-      editor: {
-        type: 'input',
-        inline: true,
-        editorSettings: {
-          autoselect: true,
-          dirtyTracker: true,
-        },
-      },
-    });
-
-    this.columns.push({
-      id: 'status',
-      name: '',
-      field: 'status',
-      resizable: true,
-      reorderable: true,
-      cssPart: (row: number) => (row < 4 || row > 4 ? 'custom-cell' : ''),
-      formatter: this.dataGrid.nativeElement.formatters.text,
-    });
-
-    this.columns.push({
-      id: 'enterpriseGroup',
-      name: 'EnterpriseGroup',
-      field: 'enterpriseGroup',
-      resizable: true,
-      reorderable: true,
-      formatter: this.dataGrid.nativeElement.formatters.integer,
-    });
-
-    this.columns.push({
-      id: 'busClassA',
-      name: 'BusClassA',
-      field: 'busClassA',
-      resizable: true,
-      reorderable: true,
-      formatter: this.dataGrid.nativeElement.formatters.text,
-    });
-
-    this.columns.push({
-      id: 'groupFieldB',
-      name: 'GroupFieldB',
-      field: 'groupFieldB',
-      resizable: true,
-      reorderable: true,
-      formatter: this.dataGrid.nativeElement.formatters.text,
-    });
-    this.columns.push({
-      id: 'parentGroupFieldB',
-      name: 'ParentGroupFieldB',
-      field: 'parentGroupFieldB',
-      resizable: true,
-      reorderable: true,
-      formatter: this.dataGrid.nativeElement.formatters.text,
-    });
-    this.columns.push({
-      id: 'name',
-      name: 'Name',
-      field: 'name',
-      resizable: true,
-      reorderable: true,
-      formatter: this.dataGrid.nativeElement.formatters.text,
-    });
-    // this.columns.push({
-    //   id: '',
-    //   name: '',
-    //   field: '',
-    //   resizable: true,
-    //   reorderable: true,
-    //   formatter: this.dataGrid.nativeElement.formatters.text,
-    // });
 
     this.dataGrid.nativeElement.editable = true;
     this.dataGrid.nativeElement.rowHeight = 'sm';
@@ -110,6 +82,6 @@ export class DataGridComponent implements AfterViewInit {
     this.dataGrid.nativeElement.rowSelection = 'multiple';
 
     this.dataGrid.nativeElement.columns = this.columns;
-    this.dataGrid.nativeElement.data = sampleData;
+    this.dataGrid.nativeElement.data = this.formattedData;
   }
 }
